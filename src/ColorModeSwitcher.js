f6c55825832250ddb4a780aaca0ed54b54bdb655
Harsh Bhatia <harshbhatia1605@gmail.com>
2021-05-02 20:43:36 +0530

    Configure dark mode and light mode

--- src/ColorModeSwitcher.js
+++ src/ColorModeSwitcher.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
 import { FaMoon, FaSun } from 'react-icons/fa';
 
-export const ColorModeSwitcher = props => {
-  const { toggleColorMode } = useColorMode();
+export const ColorModeSwitcher = (props) => {
+  const { colorMode, toggleColorMode } = useColorMode();
   const text = useColorModeValue('dark', 'light');
-  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
 
   return (
     <IconButton
-      size="md"
-      fontSize="lg"
+      size='lg'
+      fontSize='lg'
       aria-label={`Switch to ${text} mode`}
-      variant="ghost"
-      color="current"
-      marginLeft="2"
+      colorScheme='teal'
+      mr={2}
+      mt={2}
       onClick={toggleColorMode}
-      icon={<SwitchIcon />}
+      icon={colorMode === 'light' ? <FaSun /> : <FaMoon />}
+      isRound='true'
       {...props}
     />
   );
