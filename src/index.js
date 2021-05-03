a1204be35d60e0612a6455c438bd91f51b30cb6e
Harsh Bhatia <harshbhatia1605@gmail.com>
2021-05-03 10:36:29 +0530

    Refactor color mode code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-const config = {
-  initialColorMode: 'system',
-  useSystemColorMode: true,
-};
-
-const theme = extendTheme({ config });
+import theme from './theme';
 
 ReactDOM.render(
   <StrictMode>
