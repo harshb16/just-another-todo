3d10f4215d25b3dd07ff4c5fbaa156b453cd99ec
Harsh Bhatia <harshbhatia1605@gmail.com>
2021-05-03 10:35:04 +0530

    Fix color mode

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const config = {
+  initialColorMode: 'system',
+  useSystemColorMode: true,
+};
+
+const theme = extendTheme({ config });
+
 ReactDOM.render(
   <StrictMode>
-    <ChakraProvider>
-      <ColorModeScript initialColorMode='system' />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </StrictMode>,
